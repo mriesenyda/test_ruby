02c4ffa9631c994bd03553d55b134323bdb0007e
Nagesh <123335900+mriesenyda@users.noreply.github.com>
2023-12-04 18:48:42 +0300

    Create hello.rb

---
+++ hello.rb
@@ -0,0 +1,8 @@
+# Ruby program that greets the user
+
+# Ask the user for his name
+puts "Hello! What's your name?"
+name = gets.chomp
+
+# Print a greeting using the entered name
+puts "Hello, #{name}! Nice to see you."
